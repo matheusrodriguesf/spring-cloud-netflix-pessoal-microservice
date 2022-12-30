016b08219f6d21c14e18d0b17f0e67cac453a6d5
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2022-12-30 10:55:12 -0300

    Importacao Servico Base de Pagamento

---
+++ pagamento-service/src/main/java/br/com/arcelino/PagamentoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.arcelino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentoServiceApplication.class, args);
+	}
+
+}
---
+++ pagamento-service/src/test/java/br/com/arcelino/PagamentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arcelino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
