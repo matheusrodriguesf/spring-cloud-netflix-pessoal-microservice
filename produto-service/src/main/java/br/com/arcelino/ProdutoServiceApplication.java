0a978a3aa654d7c9c867d08b4411dadc5e015cbd
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2022-12-29 21:17:59 -0300

    Importacao microservice de produto

---
+++ produto-service/src/main/java/br/com/arcelino/ProdutoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.arcelino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoServiceApplication.class, args);
+	}
+
+}
---
+++ produto-service/src/test/java/br/com/arcelino/ProdutoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arcelino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
